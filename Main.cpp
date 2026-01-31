3e37511814de7df50475b4354387442c047a00f4
Pixie Zenne <231278179+pixie-zenne@users.noreply.github.com>
2026-01-31 12:19:08 +0800

    Added boards

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <cstring>
 
+uint8_t Board1[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+uint8_t Board2[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+uint8_t Board3[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+uint8_t Board4[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+uint8_t Board5[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+uint8_t Board6[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+uint8_t Board7[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+uint8_t Board8[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+uint8_t Board9[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+uint8_t BoardBuffer[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
 int main(void) {
   return 0;
 }
