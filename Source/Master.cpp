fb5f69f5b82c8f47ff622df97e8501616a2667e4
Pixie Zenne <231278179+pixie-zenne@users.noreply.github.com>
2026-03-21 09:59:03 +0800

    Many changes and debugs

--- Source/Master.cpp
+++ Source/Master.cpp
@@ -1,7 +1,6 @@
 #define master main
 
 #include <cstdbool>
-#include <cstdint>
 #include <cstring>
 #include <iostream>
 
@@ -25,12 +24,14 @@ unsigned char Mini_Board[9][9] = {
 unsigned char Player_One_Mark = 'X';
 unsigned char Player_Two_Mark = 'O';
 
-// GameState
-uint8_t Game_State-Winner = 0;
-uint8_t Game_State-Current_Player = 0;
+// Game State
+int Winner = 0;
+int Current_Player = 0;
+int Current_Mini_Board = 0;
+int Previous_Mini_Board = 0;
 
-uint8_t Legal_Turns_Played = 0;
-uint8_t Maximum_Possible_Playable_Turns = 81;
+int Legal_Turns_Played = 0;
+int Maximum_Possible_Playable_Turns = 81;
 
 /*
 The visual aid for the game.
@@ -63,12 +64,14 @@ So that the developer can easily imagine the boards if they are having a hard ti
  1 | 2 | 3    Current Player : Player Number
 --- --- ---   Current Mini Board  : Board Number
  4 | 5 | 6
---- --- ---
- 7 | 8 | 9
+--- --- ---		Select a mini board to mark
+ 7 | 8 | 9		:: 
 */
 
 void Select_First_Mini_Board(void) {
-  using namespace std
+  using namespace std;
+
+	cout << endl;
 
   cout << "   |   |     |     |   |     |     |   |   " << endl;
   cout << "--- --- ---  |  --- --- ---  |  --- --- ---" << endl;
@@ -102,14 +105,16 @@ void Select_First_Mini_Board(void) {
   cout << "   |   |      Current Player : Player One" << endl;
   cout << "--- --- ---   Current Mini Board  : None" << endl;
   cout << "   |   |   " << endl;
-  cout << "--- --- ---" << endl;
-  cout << "   |   |   " << endl;
+  cout << "--- --- ---   Please select the first board" << endl;
+  cout << "   |   |      :: ";
 
   return;
 }
 
 void Print_The_Game(void) {
-  using namespace std
+  using namespace std;
+
+	cout << endl;
 
   cout << " " << Mini_Board[0][0] << " | " << Mini_Board[0][1] << " | " << Mini_Board[0][2] << "   |   " << Mini_Board[1][0] << " | " << Mini_Board[1][1] << " | " << Mini_Board[1][2] << "   |   " << Mini_Board[2][0] << " | " << Mini_Board[2][1] << " | " << Mini_Board[2][2] << " " << endl;
   cout << "--- --- ---  |  --- --- ---  |  --- --- ---" << endl;
@@ -143,8 +148,8 @@ void Print_The_Game(void) {
   cout << " " << Main_Board[0] << " | " << Main_Board[1] << " | " << Main_Board[2] << "    Current Player : Player [Insert Number]" << endl;
   cout << "--- --- ---   Current Mini Board  : [Insert Number]" << endl;
   cout << " " << Main_Board[3] << " | " << Main_Board[4] << " | " << Main_Board[5] << endl;
-  cout << "--- --- ---" << endl;
-  cout << " " << Main_Board[6] << " | " << Main_Board[7] << " | " << Main_Board[8] << endl;
+  cout << "--- --- ---   Select a mini board to mark" << endl;
+  cout << " " << Main_Board[6] << " | " << Main_Board[7] << " | " << Main_Board[8] << "    :: ";
 
   return;
 }
@@ -159,74 +164,95 @@ void Check_Main_Board(void) {
     (Main_Board[0] == 'X' && Main_Board[1] == 'X' && Main_Board[2] == 'X') ||
     (Main_Board[3] == 'X' && Main_Board[4] == 'X' && Main_Board[5] == 'X') ||
     (Main_Board[7] == 'X' && Main_Board[7] == 'X' && Main_Board[8] == 'X')
-  ) { Game_State-Winner = 1; }
+  ) { Winner = 1; }
   else if ( // Check for matching marks in a column
     (Main_Board[0] == 'X' && Main_Board[3] == 'X' && Main_Board[6] == 'X') ||
     (Main_Board[1] == 'X' && Main_Board[4] == 'X' && Main_Board[7] == 'X') ||
     (Main_Board[2] == 'X' && Main_Board[5] == 'X' && Main_Board[8] == 'X')
-  ) { Game_State-Winner = 1; }
+  ) { Winner = 1; }
   else if ( // Check for matching marks in a diagonal
     (Main_Board[0] == 'X' && Main_Board[4] == 'X' && Main_Board[8] == 'X') ||
     (Main_Board[2] == 'X' && Main_Board[4] == 'X' && Main_Board[6] == 'X')
-  ) { Game_State-Winner = 1; }
+  ) { Winner = 1; }
 
   // FOR PLAYER TWO
   else if ( // Check for matching marks in a row
     (Main_Board[0] == 'O' && Main_Board[1] == 'O' && Main_Board[2] == 'O') ||
     (Main_Board[3] == 'O' && Main_Board[4] == 'O' && Main_Board[5] == 'O') ||
     (Main_Board[7] == 'O' && Main_Board[7] == 'O' && Main_Board[8] == 'O')
-  ) { Game_State-Winner = 2; }
+  ) { Winner = 2; }
   else if ( // Check for matching marks in a column
     (Main_Board[0] == 'O' && Main_Board[3] == 'O' && Main_Board[6] == 'O') ||
     (Main_Board[1] == 'O' && Main_Board[4] == 'O' && Main_Board[7] == 'O') ||
     (Main_Board[2] == 'O' && Main_Board[5] == 'O' && Main_Board[8] == 'O')
-  ) { Game_State-Winner = 2; }
+  ) { Winner = 2; }
   else if ( // Check for matching marks in a diagonal
     (Main_Board[0] == 'O' && Main_Board[4] == 'O' && Main_Board[8] == 'O') ||
     (Main_Board[2] == 'O' && Main_Board[4] == 'O' && Main_Board[6] == 'O')
-  ) { Game_State-Winner = 2; }
+  ) { Winner = 2; }
 
   return;
 }
 
 void Select_Mini_Board(void) {
-  // To select the next mini board
-
-  // Buffer = MiniBoard[Choice]
   return;
 };
 
 void Mark_Mini_Board(void) {
-  // Mark board here
+	int Mark = 0;
 
-  // If the move is invalid decrease by one and stay on the same mini board
-  // Legal_Turns_Played--;
+	std::cin >> Mark;
 
-  // else
-  // Select_Mini_Board();
+	if (Mark > 9 || Mark < 1 || std::cin.fail()) {
+		std::cin.clear();
+		std::cin.ignore();
+
+		std::cout << std::endl << "Invalid mark" << std::endl;
+		Legal_Turns_Played--;
+
+		return;
+	}
+
+	Previous_Mini_Board = Current_Mini_Board;
+	Current_Mini_Board = Mark;
+
   return;
 }
 
 void Mark_Main_Board(void) {
-  // More code here
   return;
 }
 
 int master(void) {
-  // Select_First_Mini_Board();
 
+  Select_First_Mini_Board();
+	std::cin >> Current_Mini_Board;
+	std::cout << Current_Mini_Board;
+
+	if (Current_Mini_Board < 1 || Current_Mini_Board > 9) {
+		std::cerr << std::endl << "Mini board selection is invalid" << std::endl;
+		std::cerr << "Please select one of the following numbers next time:" << std::endl;
+		std::cerr << "1, 2, 3, 4, 5, 6, 7, 8, or 9" << std::endl;
+		std::cerr << std::endl << "Exiting" << std::endl;
+		return 1;
+	}
+
+	// The players input are 1 to 9 while the software uses 0 to 8
+	Current_Mini_Board--;
+
+	// The actual game
   for (; Legal_Turns_Played <= Maximum_Possible_Playable_Turns; Legal_Turns_Played++) {
-
     // Show the winner of the game
-    if (Game_State-Winner == 1)  { std::cout << "Player One Wins!" << std::endl; }
-    else if (Game_State-Winner == 2) { std::cout << "Player Two Wins!" << std::endl; }
+    if (Winner == 1)  { std::cout << "Player One Wins!" << std::endl; }
+    else if (Winner == 2) { std::cout << "Player Two Wins!" << std::endl; }
 
-    Game_State-Current_Player = (i % 2) + 1;
-    Mark_Mini_Board();
-    Mark_Main_Board();
+    Current_Player = (Legal_Turns_Played % 2) + 1;
+
+		Print_The_Game();
+	  Mark_Mini_Board();
   }
 
-  std::cout << "Its a draw!" << std::endl;
+  std::cout << std::endl << "Its a draw!" << std::endl;
 
   return 0;
 }
